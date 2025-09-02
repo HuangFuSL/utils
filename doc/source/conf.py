b22a57cca9cb9805b2b948db11e7f0052c70ecf5
HuangFuSL <huangfusl@outlook.com>
2025-09-02 19:39:35 +0800

    Fix: add gymnasium to dependencies

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,10 +27,6 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-autodoc_mock_imports = [
-    'torch', 'gymnasium'
-]
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
