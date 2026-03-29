8b648bec08c9e62874f85ad14486db76f76a6948
HuangFuSL <huangfusl@outlook.com>
2026-03-29 14:16:04 +0800

    Update: Implement `SelfAttentionEmbedding`

--- ctorch/nn/embedding.py
+++ ctorch/nn/embedding.py
@@ -119,14 +119,15 @@ class FeatureEmbedding(Module):
             torch.Tensor: Tensor of shape (\\*, sum(embedding_size))
         '''
         # Sanity checks
-        if x.dim() < 2:
-            raise ValueError('Input tensor must have at least 2 dimensions.')
-        if x.shape[-1] != len(self.num_features):
-            raise ValueError(f'Input tensor must have last dimension of size {len(self.num_features)}, but got {x.shape[-1]}.')
-        if x.dtype not in (torch.int, torch.long):
-            raise TypeError('Input tensor must be of type torch.int/long.')
-        if not torch.all((x >= 0).all(dim=-1) & (x < self.num_features_tensor).all(dim=-1)):
-            raise ValueError('Input tensor contains out-of-bound indices.')
+        if self._debug:
+            if x.dim() < 2:
+                raise ValueError('Input tensor must have at least 2 dimensions.')
+            if x.shape[-1] != len(self.num_features):
+                raise ValueError(f'Input tensor must have last dimension of size {len(self.num_features)}, but got {x.shape[-1]}.')
+            if x.dtype not in (torch.int, torch.long):
+                raise TypeError('Input tensor must be of type torch.int/long.')
+            if not torch.all((x >= 0).all(dim=-1) & (x < self.num_features_tensor).all(dim=-1)):
+                raise ValueError('Input tensor contains out-of-bound indices.')
 
         # Flatten the last dim
         *shape, num_features = x.shape
@@ -144,3 +145,105 @@ class FeatureEmbedding(Module):
             ], dim=-1)
 
         return embeddings.view(*shape, -1)
+
+class SelfAttentionEmbedding(Module):
+    '''
+    An embedding layer for encoding N multiple categorical features. The module uses self-attention to perform cross-feature interaction.
+
+    Args:
+        num_features (List[int]): The number of unique values for each categorical feature.
+        embedding_size (int): The size of the embedding for each feature.
+        padding_idx (int | None): The global padding index.
+        max_norm (float | None):
+        norm_type (float):
+        scale_grad_by_freq (bool):
+        sparse (bool):
+
+    Shapes:
+
+        * Input shape: (\\*, num_features)
+        * Output shape: (\\*, embedding_size * len(num_features))
+    '''
+    def __init__(
+        self, num_features: List[int], embedding_size: int,
+        padding_idx: int | None = None, max_norm: float | None = None,
+        norm_type: float = 2.0, scale_grad_by_freq: bool = False,
+        sparse: bool = False
+    ):
+        super().__init__()
+        # Sanity checks
+        if embedding_size % 2 or embedding_size <= 0:
+            raise ValueError(f'Invalid embedding_size {embedding_size}.')
+
+        # Normalization should be done separately
+        self.num_features = num_features.copy()
+        num_features_tensor = torch.tensor(num_features, dtype=torch.long).unsqueeze(0)
+        if TYPE_CHECKING:
+            self.num_features_tensor = num_features_tensor
+        self.register_buffer('num_features_tensor', num_features_tensor)
+        self.embedding_size = embedding_size
+
+        _total_embeddings = sum(num_features + [0])
+        self.embedding = torch.nn.Embedding(
+            num_embeddings=_total_embeddings,
+            embedding_dim=embedding_size,
+            padding_idx=padding_idx,
+            scale_grad_by_freq=scale_grad_by_freq,
+            max_norm=max_norm,
+            norm_type=norm_type,
+            sparse=sparse
+        )
+
+        # Offset is used to calculate the start index of each feature's embedding
+        offset = torch.cumsum(torch.tensor([0, *self.num_features[:-1]]), dim=0, dtype=torch.long)
+        if TYPE_CHECKING:
+            self.offset = offset
+            self.sqrt: torch.Tensor
+        self.register_buffer('offset', offset)
+        self.register_buffer('sqrt', torch.tensor((embedding_size // 2) ** 0.5))
+
+    @property
+    def total_embedding_size(self) -> int:
+        '''
+        Gets the total embedding size, which is the sum of all individual embedding sizes.
+
+        Returns:
+            int: Total embedding size.
+        '''
+        return len(self.num_features) * self.embedding_size
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        '''
+        Forward pass for the embedding layer.
+
+        Args:
+            x: Tensor of shape (\\*, num_features)
+
+        Returns:
+            torch.Tensor: Tensor of shape (\\*, sum(embedding_size))
+        '''
+        # Sanity checks
+        if self._debug:
+            if x.dim() < 2:
+                raise ValueError('Input tensor must have at least 2 dimensions.')
+            if x.shape[-1] != len(self.num_features):
+                raise ValueError(f'Input tensor must have last dimension of size {len(self.num_features)}, but got {x.shape[-1]}.')
+            if x.dtype not in (torch.int, torch.long):
+                raise TypeError('Input tensor must be of type torch.int/long.')
+            if not torch.all((x >= 0).all(dim=-1) & (x < self.num_features_tensor).all(dim=-1)):
+                raise ValueError('Input tensor contains out-of-bound indices.')
+
+        # Flatten the last dim
+        *shape, num_features = x.shape
+        x = x + self.offset.expand(*shape, -1)  # Add offset to each feature's index
+
+        # Get embeddings
+        embeddings = self.embedding(x) # shape: (\\*, num_features, embedding_size)
+
+        qk, v = embeddings.chunk(2, dim=-1)
+        sa_weights = torch.softmax(
+            torch.einsum('...qe,...ke->...qk', qk, qk) / self.sqrt,
+            dim=-1
+        )
+        sa_embedding = torch.einsum('...qk,...ke->...qe', sa_weights, v)
+        return torch.cat([qk, sa_embedding], dim=-1).reshape(*shape, -1)
