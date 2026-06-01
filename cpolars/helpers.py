f0f107250d11de2a7067ae9570f1448e200751d3
HuangFuSL <huangfusl@outlook.com>
2026-06-01 19:54:06 +0800

    Update: fix helpers

--- cpolars/helpers.py
+++ cpolars/helpers.py
@@ -3,6 +3,20 @@ from typing import Sequence, TypeVar
 import polars as pl
 
 AnyFrame = TypeVar('AnyFrame', pl.DataFrame, pl.LazyFrame)
+T = TypeVar('T')
+
+Expr = pl.Expr | str
+
+def ensure_expr(expr: Expr) -> pl.Expr:
+    if isinstance(expr, str):
+        return pl.col(expr)
+    return expr
+
+def ensure_exprs(exprs: Expr | Sequence[Expr]) -> Sequence[pl.Expr]:
+    if isinstance(exprs, Sequence) and not isinstance(exprs, str):
+        return [ensure_expr(expr) for expr in exprs]
+    else:
+        return [ensure_expr(exprs)]
 
 def get_columns(df: AnyFrame) -> Sequence[str]:
     if isinstance(df, pl.LazyFrame):
@@ -17,3 +31,16 @@ def get_schema(df: AnyFrame) -> pl.Schema:
     else:
         schema = df.schema
     return schema
+
+def inspect_names(df: AnyFrame, exprs: Expr | Sequence[Expr]) -> Sequence[str]:
+    '''
+    Get the output column names of the given expressions.
+
+    Args:
+        df (pl.DataFrame | pl.LazyFrame): The input DataFrame.
+        exprs (Sequence[Expr]): The expressions to inspect.
+
+    Returns:
+        Sequence[str]: The output column names of the given expressions.
+    '''
+    return df.lazy().select(ensure_exprs(exprs)).collect_schema().names()
