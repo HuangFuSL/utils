c5b5d558c9a06020e10dd2930011a13d27403ecd
HuangFuSL <huangfusl@outlook.com>
2026-06-04 17:37:01 +0800

    Update: Add shape check for `nn.Module`

--- ctorch/nn/module.py
+++ ctorch/nn/module.py
@@ -1,8 +1,75 @@
 import warnings
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Dict, Generator, Mapping, Sequence, Tuple
 
 import torch
 
+PackedSequence = torch.nn.utils.rnn.PackedSequence
+
+TENSOR_TYPE = (torch.Tensor, PackedSequence)
+SEQUENCE_TYPE = (list, tuple)
+T_NAME = lambda x: type(x).__name__
+
+def _shape_of(obj: Any) -> Any:
+    '''
+    Helper function to get the shape of a tensor or a PackedSequence or a nested structure of them. For PackedSequence, the shape is inferred as if it were padded in batch-first format.
+
+    Args:
+        obj (Any): The object to get the shape of.
+    '''
+    if isinstance(obj, torch.Size):
+        return None # torch.Size should not appear here
+    elif isinstance(obj, torch.Tensor):
+        return obj.shape
+    elif isinstance(obj, PackedSequence):
+        batch_sizes = obj.batch_sizes
+
+        if batch_sizes.numel() == 0:
+            return torch.Size([0, 0, *obj.data.shape[1:]])
+
+        max_time = int(batch_sizes.numel())
+        batch_size = int(batch_sizes[0].item())
+        feature_shape = obj.data.shape[1:]
+
+        return torch.Size([batch_size, max_time, *feature_shape])
+    elif isinstance(obj, Mapping):
+        return {k: _shape_of(v) for k, v in obj.items()}
+    elif isinstance(obj, SEQUENCE_TYPE):
+        return [_shape_of(item) for item in obj]
+    else:
+        return None
+
+def check_shape(obj, shape) -> Generator[Tuple[str, str], None, None]:
+    if shape is None:
+        return
+    elif isinstance(shape, torch.Size):
+        obj_shape = _shape_of(obj)
+        if isinstance(obj, TENSOR_TYPE):
+            if obj_shape != shape:
+                yield '', f'Shape mismatch. Expected {shape}, got {obj_shape}'
+        else:
+            yield '', f'Type mismatch. Expected a tensor, got {T_NAME(obj)}'
+    elif isinstance(shape, Mapping):
+        if not isinstance(obj, Mapping):
+            yield '', f'Type mismatch. Expected a mapping, got {T_NAME(obj)}'
+        elif shape.keys() == obj.keys():
+            for k in shape:
+                result = check_shape(obj[k], shape[k])
+                for level, msg in result:
+                    yield f'[{k!r}]{level}', msg
+        else:
+            yield '', f'Keys do not match. Expected {list(shape.keys())}, got {list(obj.keys())}'
+    elif isinstance(shape, SEQUENCE_TYPE):
+        if isinstance(obj, TENSOR_TYPE) or not isinstance(obj, SEQUENCE_TYPE):
+            yield '', f'Type mismatch. Expected a sequence, got {T_NAME(obj)}'
+            return
+        elif len(shape) == len(obj):
+            for i, (o, s) in enumerate(zip(obj, shape)):
+                for level, msg in check_shape(o, s):
+                    yield f'[{i}]{level}', msg
+        else:
+            yield '', f'Length mismatch. Expected length {len(shape)}, got {len(obj)}'
+    else:
+        yield '', f'Type error. Unexpected type for shape check: {T_NAME(shape)}'
 
 class Module(torch.nn.Module):
     '''
@@ -17,15 +84,86 @@ class Module(torch.nn.Module):
         if TYPE_CHECKING:
             self._device_tracker: torch.Tensor
 
+    def __init_subclass__(cls) -> None:
+        super().__init_subclass__()
+        for method_name in [
+            'output_shape', 'guard_input_shape', 'guard_output_shape'
+        ]:
+            method = cls.__dict__.get(method_name, None)
+            if method is not None:
+                setattr(cls, method_name, torch.jit.unused(method))
+
+    @torch.jit.unused
+    def output_shape(
+        self, *args: torch.Size | None, **kwargs: torch.Size | None
+    ) -> torch.Size | Sequence[torch.Size] | Dict[Any, torch.Size] | None:
+        '''
+        Get the output shape of the ``module.forward()`` given input shapes. Should be implemented by subclasses if output shape checking is desired. For `PackedSequence` inputs, the shapes will be assumed to be padded in batch-first format.
+
+        Args:
+            *args (torch.Size | None): Shapes of the input tensors. None if corresponding input is not a tensor.
+            **kwargs (torch.Size | None): Shapes of any additional input tensors. None if corresponding input is not a tensor.
+
+        Returns:
+            torch.Size | Sequence[torch.Size] | Dict[Any, torch.Size] | None: The shape of the output tensor.
+        '''
+        return None # None if not perform shape checking
+
+
+    @torch.jit.unused
+    def guard_input_shape(self, *args: Any, **kwargs: Any):
+        '''
+        A hook to check the input shapes of the ``module.forward()``. Should be implemented by subclasses if input shape checking is desired.
+
+        Args:
+            *args (torch.Tensor): Input tensors.
+            **kwargs (torch.Tensor): Any additional input tensors.
+        '''
+        return None # No check by default
+
+
+    @torch.jit.unused
+    def guard_output_shape(
+        self, output_obj: Any, *input_args: Any, **input_kwargs: Any
+    ):
+        '''
+        A hook to check the output shape of the ``module.forward()``.
+
+        Args:
+            output_obj (torch.Tensor | Sequence[torch.Tensor] | Dict[Any, torch.Tensor]): The output object from the forward pass.
+            *input_args (torch.Tensor): Input tensors.
+            **input_kwargs (torch.Tensor): Any additional input tensors.
+        '''
+        output_shape = self.output_shape(
+            *[_shape_of(arg) for arg in input_args],
+            **{k: _shape_of(v) for k, v in input_kwargs.items()}
+        )
+
+        err_msg = '\n'.join([
+            f'- output{result}: {msg}'
+            for result, msg in check_shape(output_obj, output_shape)
+        ])
+        if err_msg:
+            raise ValueError(f'Output shape check failed on output of {self.__class__.__name__}:\n{err_msg}')
+
     @torch.jit.unused
     def debug(self):
         '''
         Enable debug mode (only for custom ``ctorch.nn.Modules``).
         '''
+        if self._debug:
+            return
         self._debug = True
-        for module in self.children():
+        for module in self.modules():
             if isinstance(module, Module):
                 module.debug()
+        # Attach shape check hook
+        def pre_hook(module, input_args, input_kwargs):
+            module.guard_input_shape(*input_args, **input_kwargs)
+        self.register_forward_pre_hook(pre_hook, with_kwargs=True)
+        def post_hook(module, input_args, input_kwargs, output_obj):
+            module.guard_output_shape(output_obj, *input_args, **input_kwargs)
+        self.register_forward_hook(post_hook, with_kwargs=True)
 
     @torch.jit.unused
     def _check_non_negative(self, *args: torch.Tensor):
